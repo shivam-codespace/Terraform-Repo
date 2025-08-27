1cb01214f86d3c715bb2331fb81bc18ae666bd54
Shivam Sharma <www.sharma.117@gmail.com>
2025-08-28 04:41:06 +0530

    adding 05 Terraform Script

---
+++ Seprated files/Instance Creation using variables/main.tf
@@ -0,0 +1,12 @@
+# Create multiple AWS EC2 instances using variables defined in vars.tf
+
+resource "aws_instance" "AWSServer" {
+    count         = "${var.instance_count}"
+    ami           = "${var.ami}"
+    instance_type = "${var.instance_type}"
+    key_name      = "${var.key_name}" 
+    security_groups = ["my-security-group"]
+    tags = {
+        Name = "Terraform Server - ${count.index}"
+    }
+}
